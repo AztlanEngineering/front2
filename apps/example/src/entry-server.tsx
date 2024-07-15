abfe90d09ee025e9f3bfcbd3945d3f0e164332af
advl <700@779.mx>
2024-07-15 16:56:18 +0200

    First tests

--- apps/example/src/entry-server.tsx
+++ apps/example/src/entry-server.tsx
@@ -8,23 +8,29 @@ export const config = {
   runtime: 'edge', // this is a pre-requisite
 };
 
-export default async function Handler(req: Request) {
-  let didError = false;
-
-  const stream = await renderToReadableStream(<App
-  //  req={req}
-  />, {
-    onError(err: unknown) {
-      didError = true;
-      console.error(err);
-    },
-  });
-
-
-  console.log('stream', stream);
-
-  return new Response(stream, {
-    status: didError ? 500 : 200,
-    headers: { 'Content-Type': 'text/html' },
-  });
+export default function handler(
+  request: VercelRequest,
+  response: VercelResponse,
+) {
+  return response.status(200).json({ text: 'I am an Edge Function!' });
 }
+// export default async function Handler(req: Request) {
+//   let didError = false;
+//
+//   const stream = await renderToReadableStream(<App
+//   //  req={req}
+//   />, {
+//     onError(err: unknown) {
+//       didError = true;
+//       console.error(err);
+//     },
+//   });
+//
+//
+//   console.log('stream', stream);
+//
+//   return new Response(stream, {
+//     status: didError ? 500 : 200,
+//     headers: { 'Content-Type': 'text/html' },
+//   });
+// }
