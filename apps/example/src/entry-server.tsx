7eb0fbf45c682ee02128b52fece77fde62b35dc0
advl <700@779.mx>
2024-07-15 17:16:54 +0200

    verce ljson

--- apps/example/src/entry-server.tsx
+++ apps/example/src/entry-server.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { renderToReadableStream } from 'react-dom/server.browser';
 import App from './App';
 // import { StaticRouter } from 'react-router-dom/server';
-// 
+//
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 
@@ -11,29 +11,23 @@ export const config = {
   runtime: 'edge', // this is a pre-requisite
 };
 
-export default function handler(
-  request: VercelRequest,
-  response: VercelResponse,
-) {
-  return response.status(200).json({ text: 'I am an Edge Function!' });
+export default async function Handler(req: Request) {
+  let didError = false;
+
+  const stream = await renderToReadableStream(<App
+  //  req={req}
+  />, {
+    onError(err: unknown) {
+      didError = true;
+      console.error(err);
+    },
+  });
+
+
+  console.log('stream', stream);
+
+  return new Response(stream, {
+    status: didError ? 500 : 200,
+    headers: { 'Content-Type': 'text/html' },
+  });
 }
-// export default async function Handler(req: Request) {
-//   let didError = false;
-//
-//   const stream = await renderToReadableStream(<App
-//   //  req={req}
-//   />, {
-//     onError(err: unknown) {
-//       didError = true;
-//       console.error(err);
-//     },
-//   });
-//
-//
-//   console.log('stream', stream);
-//
-//   return new Response(stream, {
-//     status: didError ? 500 : 200,
-//     headers: { 'Content-Type': 'text/html' },
-//   });
-// }
