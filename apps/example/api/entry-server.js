be155ce2e8c24452b44b1ef208f995fc17982538
advl <700@779.mx>
2024-07-15 17:28:25 +0200

    verce ljson

--- apps/example/api/entry-server.js
+++ apps/example/api/entry-server.js
@@ -1,15 +1,87 @@
-const config = {
-    runtime: 'edge',
-    // execute this function on iad1 or hnd1, based on the connecting client location
-    regions: [
-        'iad1',
-        'hnd1'
-    ]
-};
-function handler(request, response) {
-    return response.status(200).json({
-        text: `I am an Edge Function! (executed on ${process.env.VERCEL_REGION})`
+import { jsxs, Fragment, jsx } from 'react/jsx-runtime';
+import { useState } from 'react';
+import { renderToReadableStream } from 'react-dom/server.browser';
+
+const reactLogo = "/assets/react-CHdo91hT.svg";
+
+const viteLogo = "/vite.svg";
+
+function App() {
+    const [count, setCount] = useState(0);
+    return /*#__PURE__*/ jsxs(Fragment, {
+        children: [
+            /*#__PURE__*/ jsxs("div", {
+                children: [
+                    /*#__PURE__*/ jsx("a", {
+                        href: "https://vitejs.dev",
+                        target: "_blank",
+                        children: /*#__PURE__*/ jsx("img", {
+                            src: viteLogo,
+                            className: "logo",
+                            alt: "Vite logo"
+                        })
+                    }),
+                    /*#__PURE__*/ jsx("a", {
+                        href: "https://react.dev",
+                        target: "_blank",
+                        children: /*#__PURE__*/ jsx("img", {
+                            src: reactLogo,
+                            className: "logo react",
+                            alt: "React logo"
+                        })
+                    })
+                ]
+            }),
+            /*#__PURE__*/ jsx("h1", {
+                children: "Vite + React"
+            }),
+            /*#__PURE__*/ jsxs("div", {
+                className: "card",
+                children: [
+                    /*#__PURE__*/ jsxs("button", {
+                        onClick: ()=>setCount((count)=>count + 1),
+                        children: [
+                            "count is ",
+                            count
+                        ]
+                    }),
+                    /*#__PURE__*/ jsxs("p", {
+                        children: [
+                            "Edit ",
+                            /*#__PURE__*/ jsx("code", {
+                                children: "src/App.jsx"
+                            }),
+                            " and save to test HMR Change here"
+                        ]
+                    })
+                ]
+            }),
+            /*#__PURE__*/ jsx("p", {
+                className: "read-the-docs",
+                children: "Click on the Vite and React logos to learn more"
+            })
+        ]
     });
 }
 
-export { config, handler as default };
+// [REF 1.1]
+const config = {
+    runtime: 'edge'
+};
+async function Handler(req) {
+    let didError = false;
+    const stream = await renderToReadableStream(/*#__PURE__*/ jsx(App, {}), {
+        onError (err) {
+            didError = true;
+            console.error(err);
+        }
+    });
+    return new Response(stream, {
+        status: didError ? 500 : 200,
+        headers: {
+            'Content-Type': 'text/html'
+        }
+    });
+}
+
+export { config, Handler as default };
