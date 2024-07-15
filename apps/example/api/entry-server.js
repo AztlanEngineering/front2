10d9eb69cf462d6f1aa8f21eed9aa954d9656a7b
advl <700@779.mx>
2024-07-15 17:17:33 +0200

    verce ljson

--- apps/example/api/entry-server.js
+++ apps/example/api/entry-server.js
@@ -1,33 +1,88 @@
-import 'react';
+import { jsxs, Fragment, jsx } from 'react/jsx-runtime';
+import { useState } from 'react';
+import { renderToReadableStream } from 'react-dom/server.browser';
+
+const reactLogo = "/assets/react-CHdo91hT.svg";
+
+const viteLogo = "/vite.svg";
+
+function App() {
+    const [count, setCount] = useState(0);
+    return /*#__PURE__*/ jsxs(Fragment, {
+        children: [
+            /*#__PURE__*/ jsxs("div", {
+                children: [
+                    /*#__PURE__*/ jsx("a", {
+                        href: "https://vitejs.dev",
+                        target: "_blank",
+                        children: /*#__PURE__*/ jsx("img", {
+                            src: viteLogo,
+                            className: "logo",
+                            alt: "Vite logo"
+                        })
+                    }),
+                    /*#__PURE__*/ jsx("a", {
+                        href: "https://react.dev",
+                        target: "_blank",
+                        children: /*#__PURE__*/ jsx("img", {
+                            src: reactLogo,
+                            className: "logo react",
+                            alt: "React logo"
+                        })
+                    })
+                ]
+            }),
+            /*#__PURE__*/ jsx("h1", {
+                children: "Vite + React"
+            }),
+            /*#__PURE__*/ jsxs("div", {
+                className: "card",
+                children: [
+                    /*#__PURE__*/ jsxs("button", {
+                        onClick: ()=>setCount((count)=>count + 1),
+                        children: [
+                            "count is ",
+                            count
+                        ]
+                    }),
+                    /*#__PURE__*/ jsxs("p", {
+                        children: [
+                            "Edit ",
+                            /*#__PURE__*/ jsx("code", {
+                                children: "src/App.jsx"
+                            }),
+                            " and save to test HMR Change here"
+                        ]
+                    })
+                ]
+            }),
+            /*#__PURE__*/ jsx("p", {
+                className: "read-the-docs",
+                children: "Click on the Vite and React logos to learn more"
+            })
+        ]
+    });
+}
 
 // [REF 1.1]
-// import { StaticRouter } from 'react-router-dom/server';
 const config = {
     runtime: 'edge'
 };
-function handler(request, response) {
-    return response.status(200).json({
-        text: 'I am an Edge Function!'
+async function Handler(req) {
+    let didError = false;
+    const stream = await renderToReadableStream(/*#__PURE__*/ jsx(App, {}), {
+        onError (err) {
+            didError = true;
+            console.error(err);
+        }
     });
-} // export default async function Handler(req: Request) {
- //   let didError = false;
- //
- //   const stream = await renderToReadableStream(<App
- //   //  req={req}
- //   />, {
- //     onError(err: unknown) {
- //       didError = true;
- //       console.error(err);
- //     },
- //   });
- //
- //
- //   console.log('stream', stream);
- //
- //   return new Response(stream, {
- //     status: didError ? 500 : 200,
- //     headers: { 'Content-Type': 'text/html' },
- //   });
- // }
+    console.log('stream', stream);
+    return new Response(stream, {
+        status: didError ? 500 : 200,
+        headers: {
+            'Content-Type': 'text/html'
+        }
+    });
+}
 
-export { config, handler as default };
+export { config, Handler as default };
